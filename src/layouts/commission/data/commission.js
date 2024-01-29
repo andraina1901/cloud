28ccb34732c2754dc03670e66d23d0d3450e148b
andraina1901 <randrain@yahoo.com>
2024-01-29 14:33:51 +0300

    with co

---
+++ src/layouts/commission/data/commission.js
@@ -0,0 +1,19 @@
+const commission = {
+    columns: [
+        { name: "commission", align: "center" },
+        { name: "dateheure", align: "center" },
+      ],
+      rows: [
+        {
+            commission: "50",
+            dateheure: "20/01/2023",
+        },
+        {
+            commission: "20",
+            dateheure: "20/02/2024",
+        },
+      
+      ]
+}
+
+export default commission;
--- src/layouts/commission/index.js
+++ src/layouts/commission/index.js
@@ -2,13 +2,40 @@ import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Table from "examples/Tables/Table";
+import commission from "./data/commission";
+import { Button, TextField, Typography } from "@mui/material";
+import SoftButton from "components/SoftButton";
 
 function Commission() {
+    
+  const { columns, rows } = commission;
     return(
         <DashboardLayout>
             <DashboardNavbar/>
             <SoftBox>
-                <SoftTypography>Commission</SoftTypography>
+                    <form>
+                        
+                        <SoftTypography fontWeight="bold" style={{fontSize: '20px'}}>Modifier de la commission</SoftTypography>
+                        <SoftBox mb={3}  display="flex" style={{gap: '5%'}}>
+                            <SoftBox>
+                                <SoftTypography style={{fontSize: '13px'}}>Commission</SoftTypography>
+                                <TextField type="text" />
+                            </SoftBox>
+                            <SoftBox>
+                                <SoftTypography style={{fontSize: '13px'}}>Date</SoftTypography>
+                                <TextField type="date" />
+                            </SoftBox>
+                            <SoftBox mt={3}>
+                                <SoftButton variant="gradient" color="dark" >
+                                &nbsp;Modifier
+                                </SoftButton>
+                            </SoftBox>
+                        </SoftBox>
+                    </form>
+                <SoftBox>
+                    <Table columns={columns} rows={rows}/>
+                </SoftBox>
             </SoftBox>
         </DashboardLayout>
 
